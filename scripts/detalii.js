60e20a4118e69076e10dcb21a07e798ce128f30c
nandreiv <nedea.andreiv@gmail.com>
2018-12-15 14:53:47 +0200

    Lucrari in lemn v1.0.0

--- scripts/detalii.js
+++ scripts/detalii.js
@@ -5,36 +5,25 @@ function getProjects() {
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       imagini = JSON.parse(this.responseText);
-      //drawTable();
+      drawTable();
     }
   };
   xhttp.open("GET", `https://lucrari-in-lemn.firebaseio.com/${idx}.json`, true)
   xhttp.send();
 }
 
-function drawTable() {
-  var str = "";
+function drawTable (){
 
-  for (var i = 0; i < imagini.length; i++) {
-    if (i != null) {
-      str += `<tr>
-                <td><b>${imagini[i].nume}</b></td>
-                <td><img src="${imagini[i].imagine1}"/></td>
-                <td><img src="${imagini[i].imagine2}"/></td>
-                <td><img src="${imagini[i].imagine3}"/></td>
-                <td><img src="${imagini[i].imagine4}"/></td>
-                <td><img src="${imagini[i].imagine5}"/></td>
-                <td><img src="${imagini[i].imagine6}"/></td>
-                <td><img src="${imagini[i].imagine7}"/></td>
-                <td><img src="${imagini[i].imagine8}"/></td>
-                <td><img src="${imagini[i].imagine9}"/></td>
-                <td><img src="${imagini[i].imagine10}"/></td>
-                <td><img src="${imagini[i].imagine11}"/></td>
-                <td><img src="${imagini[i].imagine12}"/></td>
-              </tr>`;
-    }
+  for (var i in imagini) {
+
+   document.querySelector("#imageShow").insertAdjacentHTML (
+    'afterbegin',
+    `<div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" src="${imagini[i].imagine1}">
+        </div>
+      </div>`
+   )
   }
-
-  document.querySelector("table tbody").innerHTML = str;
 }
 
