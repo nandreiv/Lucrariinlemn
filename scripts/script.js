245d48e5aa4b12b9d71098d6b93408cd2d76fbe5
nandreiv <nedea.andreiv@gmail.com>
2018-12-15 02:16:45 +0200

    Lucrari in lemn V1.0.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,34 @@
 
 var imagini = [];
 
-function getProject() {
+function getProjects() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       imagini = JSON.parse(this.responseText);
-      //drawTable();
+      drawCards();
     }
   };
   xhttp.open("GET", `https://lucrari-in-lemn.firebaseio.com/.json`, true)
   xhttp.send();
 }
 
+function drawCards (){
 
-function clickButton () {
-  for (var i = 0; i < imagini.length; i++) {
-  window.location=`detalii.html?id=${i}`;
+  for (var i in imagini) {
+
+   document.querySelector("#contentProjects").insertAdjacentHTML (
+    'afterend',
+    `<div class="col-md-4">
+                    <div class="card">
+                      <img class="card-img-top" src="${imagini[i].imagine1}">
+                      <div class="card-body">
+                        <h4 class="card-title">${imagini[i].nume}</h4>
+                        <button  class="btn btn-outline-secondary" onclick="window.location='detalii.html?id=${i}'">Vezi proiect</button>
+                      </div>
+                      </div>
+                    </div>`
+   )
   }
 }
+     
